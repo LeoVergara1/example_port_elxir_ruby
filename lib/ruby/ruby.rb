cf4f9f9540501a589080f15e570fae020ea3d6e8
Brandon Vergara <brandon@makingdevs.com>
2018-05-25 11:11:42 -0500

    reading from epub

--- lib/ruby/ruby.rb
+++ lib/ruby/ruby.rb
@@ -1,3 +1,5 @@
+require 'epub/parser'
+
 def hello_world
   puts "Hello from Ruby esto en ruby"
 end
@@ -5,3 +7,15 @@ end
 def pass_to_param(param)
   puts "Esté llego #{param}"
 end
+
+def read_epub
+  book = EPUB::Parser.parse('elixir-book.epub')
+  book.metadata.title
+  l = []
+  book.each_page_on_spine do |page|
+    l << page.read
+  end
+  l
+end
+
+
--- lib/ruby_call.ex
+++ lib/ruby_call.ex
@@ -12,4 +12,10 @@ defmodule RubyElixir.RubyCall do
     |> Ruby.call("ruby", "pass_to_param", ["hola ruby"])
   end
 
+  def read_epub_from_elixir do
+    {:ok, ruby} = Ruby.start(ruby_lib: Path.expand("lib/ruby"))
+    ruby
+    |> Ruby.call("ruby", "read_epub", [])
+  end
+
 end
